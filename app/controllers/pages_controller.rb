027c10395c5593af93adc7d2d31b7ddb87479d0e
Dave Peiris <dave@sharkseo.com>
2016-12-01 12:20:16 +0000

    First

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+
+  def homepage
+    @title = "SEO Consultant & Ruby Developer: Dave Peiris"
+    @description = "Dave Peiris is a search marketing consultant and software engineer specialising in technical SEO audits for enterprise and retail websites"
+  end
+
+end
